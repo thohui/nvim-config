a5f798094a2d9ffeecea02d75d788a5e351810c3
Thomas <contact@thohui.com>
2023-05-28 15:49:20 +0200

    Update nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -11,7 +11,7 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup({
-	open_on_setup = false,
+	on_attach = on_attach, 
 	hijack_directories = {
 		enable = false,
 	},
@@ -60,12 +60,22 @@ nvim_tree.setup({
 	view = {
 		width = 40,
 		side = "left",
-		mappings = {
-			list = {
-				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-				{ key = "h", cb = tree_cb("close_node") },
-				{ key = "v", cb = tree_cb("vsplit") },
-			},
-		},
 	},
 })
+
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+ 
+  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+
+end
